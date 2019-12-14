82eef9dc21aeac31c1a1f7a487fe1a672c78f8f4
Tim <wayfarer1313@gmail.com>
2019-12-14 16:33:34 -0600

    added key and post

--- www/studentAppProcessor.php
+++ www/studentAppProcessor.php
@@ -164,16 +164,13 @@ $curl_payload = ""
   /**************************
   Make CURL post
   ******************/
-  $data = array("name" => "Hagrid", "age" => "36");
-  $data_string = json_encode($data);
-
-  $ch = curl_init('http://api.local/rest/users');
+  $postUrl = getenv('SLACKBOT_POST_KEY');
+  $ch = curl_init($postUrl);
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $curl_payload);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
       'Content-Type: application/json',
-      'Content-Length: ' . strlen($data_string))
   );
 
   $result = curl_exec($ch);
