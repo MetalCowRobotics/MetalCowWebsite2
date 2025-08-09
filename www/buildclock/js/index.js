4039dfd241f491090ac0ca26c11a4bde9c30559c
Tim <tim@metalcowrobotics.com>
2025-08-09 10:15:41 -0500

    Update Build Clock
    updated for 2026 dates

--- www/buildclock/js/index.js
+++ www/buildclock/js/index.js
@@ -7,13 +7,13 @@
 // example: raceDate = new Date(2003,03,26,14,15,00) = April 26, 2003 - 2:15:00 pm
 
 
-//KICKOFF: Saturday, January 4, 2020 | 10:00 am - 12:00 pm ET
-//This means Jan 4, 2020 @ 9am CST
-startBuild = new Date(2020,0,4,8,30,0);
+//KICKOFF: Saturday, January 10, 2026 | 10:00 am - 12:00 pm ET
+//This means Jan 10, 2020 @ 9am CST
+startBuild = new Date(2026,0,10,8,30,0);
 
-//STOP BUILD: February 20, 2018 - 11:59pm ET
-//This means Feb 19, 2020 @ 10:59 CST
-stopBuild = new Date(2020,1,19,21,59,0); //Stop Build 2018
+//STOP BUILD: February 21, 2026 - 11:59pm ET
+//This means Feb 21, 2026 @ 10:59 CST
+stopBuild = new Date(2026,1,21,21,59,0); //Stop Build 2018
 
 function getDiff(ddate){
 	dateNow = new Date();	//grab current date
